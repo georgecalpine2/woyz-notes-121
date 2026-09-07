7ae3c0938c7231896165c2c7733262ef350626d4
George Abraham <georgeabraham@George-MacBook-Pro.local>
2026-09-07 13:44:05 +0530

    Configure new Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyA_LoTqVtGx6LTokFO5BVXpPfVw4y3YxAI",
-  "authDomain": "woyz-notes-20260807073630-6071.firebaseapp.com",
-  "projectId": "woyz-notes-20260807073630-6071",
-  "storageBucket": "woyz-notes-20260807073630-6071.firebasestorage.app",
-  "messagingSenderId": "403388564806",
-  "appId": "1:403388564806:web:5547570896156e4b339147"
+  "apiKey": "AIzaSyAcRHPEWDJ65ue4Z9ixybQWk6tGNgFgoJ4",
+  "authDomain": "woyz-notes-20260907080258-0e21.firebaseapp.com",
+  "projectId": "woyz-notes-20260907080258-0e21",
+  "storageBucket": "woyz-notes-20260907080258-0e21.firebasestorage.app",
+  "messagingSenderId": "1097401054054",
+  "appId": "1:1097401054054:web:ffb526da5c44c3f4394ea7"
 };
